4a8e1da1beb346430c001be3fc929722ce8e0993
KianFatemi <kianfatemi044@gmail.com>
2025-08-25 11:51:58 -0400

    added tray application class and config file

---
+++ TrayApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Bson;
+
+namespace WindowDesktopSwitcher
+{
+    class TrayApplicationContext : ApplicationContext
+    {
+        NotifyIcon trayIcon;
+
+        public TrayApplicationContext()
+        {
+            trayIcon = new NotifyIcon()
+            {
+                Icon = System.Drawing.SystemIcons.Application,
+                ContextMenuStrip = new ContextMenuStrip()
+                {
+                    Items = { new ToolStripMenuItem("Exit", null, Exit) }
+                },
+                Visible = true
+            };
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
