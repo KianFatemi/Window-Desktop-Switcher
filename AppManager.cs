27c6b5692392d8a357bdcee442d6bf03460576e3
KianFatemi <kianfatemi044@gmail.com>
2025-08-25 14:28:17 -0400

    added two classes to switch desktop and app

---
+++ AppManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowDesktopSwitcher
+{
+    class AppManager
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        const int SW_RESTORE = 9;
+
+        public void FocusOrLaunch(string exePath)
+        {
+            try
+            {
+                string processName = Path.GetFileNameWithoutExtension(exePath);
+                var process = Process.GetProcessesByName(processName).FirstOrDefault();
+
+                if (process != null)
+                {
+                    IntPtr handle = process.MainWindowHandle;
+                    if (handle != IntPtr.Zero)
+                    {
+                        ShowWindow(handle, SW_RESTORE);
+                        SetForegroundWindow(handle);
+                    }
+                }
+                else
+                {
+                    Process.Start(exePath);
+                }
+            }
+            catch ( Exception ex)
+            {
+                MessageBox.Show($"Error handling application '{exePath}':\n{ex.Message}", "App Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ DesktopManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsDesktop;
+
+
+namespace WindowDesktopSwitcher
+{
+    class DesktopManager
+    {
+        public void SwitchDesktops(int desktopNumber)
+        {
+            int targetIndex = desktopNumber - 1;
+            if (targetIndex < 0) return;
+
+            var desktops = VirtualDesktop.GetDesktops();
+            if (targetIndex < desktops.Length)
+            {
+                desktops[targetIndex].Switch();
+            }
+        }
+    }
+}
