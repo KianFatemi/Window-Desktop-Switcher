98489d82676f9a508bf85a9b1ab49f669cccbbcf
KianFatemi <kianfatemi044@gmail.com>
2025-09-02 14:25:21 -0400

    Implemented automatic desktop creation

--- Managers/DesktopManager.cs
+++ Managers/DesktopManager.cs
@@ -12,13 +12,31 @@ namespace WindowDesktopSwitcher.Managers
     {
         public void SwitchDesktops(int desktopNumber)
         {
-            int targetIndex = desktopNumber - 1;
-            if (targetIndex < 0) return;
-
-            var desktops = VirtualDesktop.GetDesktops();
-            if (targetIndex < desktops.Length)
+            try
             {
-                desktops[targetIndex].Switch();
+                int targetIndex = desktopNumber - 1;
+                if (targetIndex < 0) return;
+
+                var desktops = VirtualDesktop.GetDesktops();
+                int currentDesktopCount = desktops.Length;
+
+                if (desktopNumber > currentDesktopCount)
+                {
+                    int desktopsToCreate = desktopNumber - currentDesktopCount;
+                    for (int i = 0; i < desktopsToCreate; i++)
+                    {
+                        VirtualDesktop.Create();
+                    }
+                }
+                var finalDesktops = VirtualDesktop.GetDesktops();
+                if (targetIndex < finalDesktops.Length)
+                {
+                    desktops[targetIndex].Switch();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in SwitchToDesktop: {ex.Message}");
             }
         }
     }
